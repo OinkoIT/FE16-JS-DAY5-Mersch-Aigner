6f9bf2dd43f710b18d083029c39be899a7538cdb
OinkoIT <claus.mersch@gmail.com>
2022-06-02 11:10:05 +0200

    basic ex2

--- Aigner/01-basic/script-b.js
+++ Aigner/01-basic/script-b.js
@@ -100,3 +100,25 @@ const person3 = {
   },
 };
 document.querySelector(".basic-ex1").innerHTML += person3.fullName() + "<br>";
+
+// Ex2
+/*Based on the following two Objects definitions:
+
+var sandwiches = `{ "sandwich": "hamburger", "calories": "260" }`;
+var fries = `{ "fries_size": "Large French Fries", "calories": "570" }`;
+print the following message within the browser: My favorite sandwich is a Hamburger which has approximately 260 calories, along with it I enjoy eating Large French Fries which have about 570 calories.*/
+
+// var sandwiches = { sandwich: "hamburger", calories: 260 };
+// var fries = { fries_size: "Large French Fries", calories: 570 };
+
+var sandwiches = `{ "sandwich": "hamburger", "calories": "260" }`;
+var fries = `{ "fries_size": "Large French Fries", "calories": "570" }`;
+
+var sandwichObj = JSON.parse(sandwiches);
+var friesObj = JSON.parse(fries);
+
+console.log(sandwichObj);
+console.log(friesObj);
+
+document.querySelector(".basic-ex1").innerHTML += `My favorite sandwich is a ${sandwichObj.sandwich} which has approximately ${sandwichObj.calories} calories, along with it I enjoy eating ${friesObj.fries_size} which have about ${friesObj.calories} calories. <br>`;
+
