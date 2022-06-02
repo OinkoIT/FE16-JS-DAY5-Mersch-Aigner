dd6991e2d27bda711f6552819fce1700edcb234c
OinkoIT <claus.mersch@gmail.com>
2022-06-02 16:06:05 +0200

    minor updates

--- Aigner/04-challange/script-c.js
+++ Aigner/04-challange/script-c.js
@@ -9,22 +9,22 @@ const preis = document.querySelectorAll(".preis");
 const pic = document.querySelectorAll(".pic");
 const carName = document.querySelectorAll(".name");
 
-const printCar = () => {
-    car.forEach ((item, i) => {
-        baujahr[i].innerHTML = `Baujahr: ${car[i].year}`;
-        preis[i].innerHTML = `Preis: ${car[i].price} €`;
-        carName[i].innerHTML = `${car[i].carName}`;
-        pic[i].src = `${car[i].image}`;
-    })
-};
-
 // const printCar = () => {
-//     for (let i = 0; i < usersList.length; i++) {
-//       baujahr[i].innerHTML = `Baujahr: ${car[i].year}`;
-//       preis[i].innerHTML = `Preis: ${car[i].price}`;
-//       carName[i].innerHTML = `${car[i].carName}`;
-//       pic[i].src = `${car[i].image}`;
-//     }
-//   };
+//     car.forEach ((item, i) => {
+//         baujahr[i].innerHTML = `Baujahr: ${car[i].year}`;
+//         preis[i].innerHTML = `Preis: ${car[i].price} €`;
+//         carName[i].innerHTML = `${car[i].carName}`;
+//         pic[i].src = `${car[i].image}`;
+//     })
+// };
+
+const printCar = () => {
+    for (let i = 0; i < car.length; i++) {
+      baujahr[i].innerHTML = `Baujahr: ${car[i].year}`;
+      preis[i].innerHTML = `Preis: ${car[i].price}`;
+      carName[i].innerHTML = `${car[i].carName}`;
+      pic[i].src = `${car[i].image}`;
+    }
+  };
 
 printCar();
